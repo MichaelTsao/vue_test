039b703fab8873caa45d3db4ac4961a1468cf4ef
caoxiang <caoxiang@yeah.net>
2019-07-29 18:08:21 +0800

    init

---
+++ src/components/ScheduleForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-dialog max-width="500px" v-model="show">
+        <v-card>
+            <v-card-actions>
+                <v-btn @click.stop="show=false" color="primary" flat>Close</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    export default {
+        props: {
+            value: Boolean
+        },
+        computed: {
+            show: {
+                get() {
+                    return this.value
+                },
+                set(value) {
+                    this.$emit('input', value)
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SearchText.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-menu offset-y v-model="showMenu.company">
+        <template v-slot:activator="{ on }">
+            <v-text-field
+                    @keyup="queryForKeywords('company', 'editedItem.company', $event)"
+                    label="公司"
+                    v-model="editedItem.company"
+            ></v-text-field>
+        </template>
+        <v-list>
+            <v-list-tile
+                    :key="index"
+                    @click="setKeyword('company', 'editedItem.company', index)"
+                    v-for="(item, index) in suggestKeywords"
+            >
+                <v-list-tile-title>{{ item }}</v-list-tile-title>
+            </v-list-tile>
+        </v-list>
+    </v-menu>
+</template>
+
+<script>
+    export default {
+        name: "SearchText",
+        props: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
