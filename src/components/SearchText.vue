62e7b954b7cf9d8900d7634ca667dd69d09661fd
caoxiang <caoxiang@yeah.net>
2019-07-30 18:38:21 +0800

    more

--- src/components/SearchText.vue
+++ src/components/SearchText.vue
@@ -1,17 +1,17 @@
 <template>
-    <v-menu offset-y v-model="showMenu.company">
+    <v-menu offset-y v-model="showMenu">
         <template v-slot:activator="{ on }">
             <v-text-field
-                    @keyup="queryForKeywords('company', 'editedItem.company', $event)"
-                    label="公司"
-                    v-model="editedItem.company"
+                    :label="this.name"
+                    @keyup="queryForKeywords()"
+                    v-model="this.value"
             ></v-text-field>
         </template>
         <v-list>
             <v-list-tile
+                    v-for="(item, index) in suggestKeywords"
                     :key="index"
                     @click="setKeyword('company', 'editedItem.company', index)"
-                    v-for="(item, index) in suggestKeywords"
             >
                 <v-list-tile-title>{{ item }}</v-list-tile-title>
             </v-list-tile>
@@ -22,7 +22,40 @@
 <script>
     export default {
         name: "SearchText",
-        props: {}
+        props: ['type', 'name', 'value'],
+        data: () => ({
+            showMenu: false,
+        }),
+        methods: {
+            queryForKeywords: function () {
+                let that = this;
+
+                let value = eval('this.' + key);
+                if (typeof value == 'undefined') {
+                    value = '';
+                }
+                this.$http.get(process.env.VUE_APP_API_URL + '/friends/suggest/' + type + '/' + encodeURI(value) + '?access-token=100-token')
+                    .then(function (response) {
+                        that.suggestKeywords = response.data;
+                        // console.log(response);
+
+                        if (typeof response.data !== 'undefined' && response.data.length > 0) {
+                            that.$nextTick(() => {
+                                eval('that.showMenu.' + type + ' = true');
+                            });
+                        } else {
+                            eval('that.showMenu.' + type + ' = false');
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            },
+
+            setKeyword: function (type, key, index) {
+                eval("this." + key + "='" + this.suggestKeywords[index] + "'");
+            },
+        }
     }
 </script>
 
